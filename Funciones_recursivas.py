2b1127c9d04d3d2afef9c4da2280956d8a8b0296
rcdelvalle <rcdelvalle2017@gmail.com>
2023-09-14 17:12:02 -0300

    Ejercicio 1 mejorado

--- Funciones_recursivas.py
+++ Funciones_recursivas.py
@@ -19,41 +19,39 @@ El total vendido por toda la cadena
 # Genera una lista vacía
 lista = []
 nro_suc = int(input("Nro de sucursal: "))
+# El total vendido por toda la cadena
+total_cadena = 0
 
 while (nro_suc != 0):
     nombre = input("Nombre de sucursal: ")
     ventas = int(input("Cantidad de ventas: "))
     total = int(input("Venta total de la sucursal: "))
+    total_cadena += total
 
     # append nro suc, nombre, ventas, total
     # llenando la lista según lo que ingrese el usuario.
-    lista.append(nro_suc)
-    lista.append(nombre)
-    lista.append(ventas)
-    lista.append(total)
+    lista.append([nro_suc, nombre, ventas, total])
+    
 
     # vuelvo a solicitar el nro suc
     nro_suc = int(input("Nro de sucursal: "))
 
-cantidad = 0
-# bucle a imprimir
-for i in lista:
-    if(cantidad % 4 == 0):
-        print(f"Nro Sucursal: {i}")
-    elif (cantidad % 4 == 1):
-        print(f"Sucursal: {i}")
-    elif (cantidad % 4 == 2):    
-        print(f"Ventas: {i}")
-        ventas = i
-    else:
-        print(f"Total sucursal: {i}")
-        total = i
-        
-        # Se deberá sacar el promedio de ventas en pesos de esa sucursal
-        promedio = total / ventas
-        print(f"Promedio: {promedio}")
 
-    cantidad += 1
+# bucle a imprimir
+for suc in lista:
+    
+    print(f"nro Sucursal: {suc[0]}")
+    print(f"Sucursal: {suc[1]}")
+    print(f"Ventas: {suc[2]}")
+    print(f"Total sucursal: {suc[3]}")
+        
+    # Se deberá sacar el promedio de ventas en pesos de esa sucursal
+    promedio = suc[3] / suc[2]
+    print(f"Promedio: {promedio}")
+
+print(f"Total cadena:{total_cadena}")
+
+    
     
     
 
